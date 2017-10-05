cdaa2790a97258752bae056337ee70b5919d8d6f
Alex <alex@Mac-mini-de-Alex.local>
2017-10-05 13:38:05 -0300

    primeiro commit

--- MAZap/app/src/main/java/br/com/ma/android/mazap/activity/LoginActivity.java
+++ MAZap/app/src/main/java/br/com/ma/android/mazap/activity/LoginActivity.java
@@ -1,15 +1,111 @@
 package br.com.ma.android.mazap.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.ma.android.mazap.R;
+import br.com.ma.android.mazap.helper.FireBaseConfiguracoes;
+import br.com.ma.android.mazap.modelo.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText email;
+    private EditText senha;
+    private Button btnLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.txt_email);
+        senha = (EditText) findViewById(R.id.txt_senha);
+        btnLogar = (Button) findViewById(R.id.btn_logar);
+
+        verificarUsuarioLogado();
+
+
+        btnLogar.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                if (email.getText().length() > 0 && senha.getText().length() > 0) {
+                    usuario = new Usuario();
+                    usuario.setEmail(email.getText().toString());
+                    usuario.setSenha(senha.getText().toString());
+                    validarLogin();
+                } else {
+                    Toast.makeText(LoginActivity.this, "E-mail e senha precisam ser preenchidos!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
     }
+
+    private void validarLogin() {
+        autenticacao = FireBaseConfiguracoes.autenticacaoFirebase();
+        autenticacao.signInWithEmailAndPassword(
+                this.usuario.getEmail(),
+                this.usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    abrirTelaInicial();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Erro ao logar, tente novamente!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Verifica se o usuario está logado e se não estiver, carrega a tela de logion normalmente
+     * se o mesmo estiver logado, será redirecionado para a tela principal
+     */
+    public void verificarUsuarioLogado(){
+        autenticacao = FireBaseConfiguracoes.autenticacaoFirebase();
+        if (autenticacao.getCurrentUser() != null){
+            abrirTelaInicial();
+        }
+    }
+
+    /**
+     * Abre a tela principal
+     */
+    private void abrirTelaInicial() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Abre a tela de cadastro
+     * @param view
+     */
+    public void abrirTelaCadastro(View view){
+
+        Intent intent = new Intent(LoginActivity.this, CadastrarActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
 }
--- MAZap/app/src/main/java/br/com/ma/android/mazap/activity/MainActivity.java
+++ MAZap/app/src/main/java/br/com/ma/android/mazap/activity/MainActivity.java
@@ -1,24 +1,70 @@
 package br.com.ma.android.mazap.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.ma.android.mazap.R;
+import br.com.ma.android.mazap.helper.FireBaseConfiguracoes;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    //private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+    private Toolbar mnToolbar;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //referenciaFirebase.child("pontos").setValue(100);
+        mnToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mnToolbar.setTitle("MAZap");
+        setSupportActionBar( mnToolbar );
+
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    private void abrirTelaLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.item_sair:
+                sair();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void sair(){
+        autenticacao = FireBaseConfiguracoes.autenticacaoFirebase();
+        autenticacao.signOut();
+        abrirTelaLogin();
+    }
+
+
 }
